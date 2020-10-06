86fb8e00e133abc3e268ca2110c2cefbe8c8ae0e
vimalrajan242001 <vimalrajansret@gmail.com>
2020-10-06 18:31:23 +0530

    dynamic bg for current weather added

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="hello">
       <el-tabs v-model="activeName" @tab-click="handleClick">
+  <el-tab-pane label="Current" name="Current">  
+            <div id="app" :class="typeof weather.main != 'undefined' && (weathers == null) ? 'null' : 
+                                                                        (weathers == 'Clouds') ? 'cloud' : 
+                                                                        (weathers == 'Clear') ? 'clear' : 
+                                                                        (weathers == 'Haze') ? 'cloud' :
+                                                                         (weathers == 'Thunderstorm') ? 'thunder' :
+                                                                        (weathers == 'Rain') ? 'rain' : 'not found'">
+    <main>
+      <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+      <!-- <el-row>
+  <el-col :span="8" >
+   <el-card :body-style="{ padding: '0px' }"> -->
+       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ weather.main.temp }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    <!-- </el-card>
+  </el-col>
+      </el-row> -->
+    </main>
+  </div>
+      </el-tab-pane>
     <el-tab-pane label="Summary" name="Summary">
           <div class="container">
           <el-col :span="24" class="toolbar">
@@ -91,6 +128,7 @@
       </div>
     </div>
       </el-tab-pane>
+    
   </el-tabs>    
 
 
@@ -105,7 +143,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      activeName: 'Summary',
+      activeName: 'Current',
       input2:null,
       show:false,
       gridData:[],
@@ -116,7 +154,12 @@ export default {
       dates: [],
       temps: [],
       loading: false,
-      errored: false
+      errored: false,
+      api_key: 'fd3150a661c1ddc90d3aefdec0400de4',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {},
+      weathers: null,
     }
   },
   methods: {
@@ -245,6 +288,32 @@ export default {
           this.errored = true;
         })
         .finally(() => (this.loading = false));
+    },
+
+      fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+      this.weathers = this.weather.weather[0].main ;
+      console.log(this.weathers)
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     }
     },
    
@@ -332,4 +401,101 @@ a {
     padding: 10px 0;
     background-color: #f9fafc;
   }
+  
+#app.cloud {
+  background-image: url('../assets/cloudy.jpg');
+}
+#app.clear {
+  background-image: url('../assets/clear.jpg');
+}
+#app.rain {
+  background-image: url('../assets/rain.jpg');
+}
+#app.thunder {
+  background-image: url('../assets/thunder.jpg');
+}
+#app.null {
+  background-image: url('../assets/null.png');
+}
+
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 </style>
